69f49211efbc1d9a7f910416985bb190f98324c4
Sosuke Brause <brosuke89@gmail.com>
2020-05-22 04:56:50 -0700

    add time to main card and side list and localstorage

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
     input = $("#input").val();
     $("#input").val("");
     $(".card-container").empty();
-
     getWeather();
+    searchList();
   });
 
   function getWeather() {
     $.ajax({
       type: "GET",
-      url: `https://api.openweathermap.org/data/2.5/weather?zip=${input}&appid=c1dc5cf44c3a5ebaadb89b904175bcca`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=c1dc5cf44c3a5ebaadb89b904175bcca`,
       dataType: "JSON",
     }).then(function (response) {
       console.log(response);
@@ -20,31 +20,45 @@ $(document).ready(function () {
       let humid = response.main.humidity;
       let tempF = convert(response.main.temp);
       let fl = convert(response.main.feels_like);
-      let maxtemp = convert(response.main.temp_max);
-      let mintemp = convert(response.main.temp_min);
+      let max = convert(response.main.temp_max);
+      let min = convert(response.main.temp_min);
       function convert(n) {
         var convertedLocal = (1.8 * (n - 273.15) + 32).toFixed(1);
-        console.log(convertedLocal);
         return convertedLocal;
       }
 
-      cardTemplate = $(".card-container").append(
-        `<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-              <div class="card-header"></div>
-              <div class="card-body">
-              <h5 class="card-title">Info card title</h5>
-              <p class="card-text">Some quick example text.</p>
-              </div>
-          </div>`
-      );
-      var humidity = $(`<h5>`).text(humid + "% humidity; " + desc);
-      var thefeels = $(`<h5>`).text("Feels like " + fl + "°");
-      var where = $(`<h4>`).text(response.name + ": " + tempF + " °F");
-      var range = $(`<h5>`).text(
-        "Low/High: " + mintemp + " ~ " + maxtemp + "°; "
-      );
-      $(".card-header").prepend(where);
-      $(".card-body").prepend(thefeels, humidity, range);
+      cardTemplate = $(".card-container").append(`
+        <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
+          <div class="card-footer"></div>
+          </div>
+        </div>`);
+
+      var humidity = $(`<h6>`).text(humid + "% humidity; " + desc);
+      var thefeels = $(`<h6>`).text("Feels like " + fl + "°");
+      var range = $(`<p>`).text("Min/Max: " + min + " ~ " + max + "°");
+      $(".card-header").prepend(response.name);
+      $(".card-footer").prepend(moment().format("lll"));
+      $(".card-text").prepend("Currently " + tempF + "°");
+      $(".card-text").append(thefeels, humidity, range);
+
+      function searchList(city) {
+        $(".list-group").append(`
+        <li class="list-group-item">${city}</li>`);
+        window.localStorage.setItem("history", JSON.stringify(city));
+      }
+      searchList($(".card-header").text());
     });
   }
+  $("#time").text(moment().format("LT"));
 });
+
+// function makeRow(city) {
+//   var li = $("<li>").addClass("list-group-item").text(city);
+//   $(".list-group").append(li);
+//   console.log($(".card-header").text());
+// }
+// makeRow($(".card-header").text());
